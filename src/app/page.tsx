b7b1098e222cb97c036056eb9488f8ce5962899a
Ibrahim Uzun <info@ibrahimuzun.com>
2023-06-07 00:38:28 +0300

    Add test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (<div>
     <h1>Next.js API Routes for Restful API</h1>
     <p>This template is a starting point for building a RESTful API with Next.js API Routes.</p>
     <p>To start, edit <code>src/app/api/route.ts</code>.</p>
+    <div>
+
+    To test the API, click on the links below: <br />
+      <Link href="/api">
+        <strong>
+          Test API
+        </strong>
+      </Link>
+    </div>
   </div>)
 }
